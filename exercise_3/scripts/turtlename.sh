aa4985c0a295062d8ed3913d139b5f1c54d2205b
Alexander J. Magnusson Amoros <aj.magnussson@gmail.com>
2021-11-25 15:00:59 +0100

    Retouched and polished

--- exercise_3/scripts/turtlename.sh
+++ exercise_3/scripts/turtlename.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 rosservice call /turtle1/teleport_absolute 5.5 5.5 0
 rosservice call /clear
-
+#movement for J
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, .0, 0.0]' '[0.0, 0.0, -1.5]'
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[2.0, 0.0, 0.0]' '[0.0, 0.0, 3.0]'
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[3.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
+#position turtle
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, 1.5]'
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[8.0, 0.0, 0.0]' '[0.0, 0.0, 7.5]'
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[8.0, 0.0, 0.0]' '[0.0, 0.0, 7.5]'
+#position turtle
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, 0.7]'
-
-rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[-0.62, -0.5, 0.0]' '[0.0, 0.0, 0.0]'
-rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[1.24, 1.0, 0.0]' '[0.0, 0.0, 0.0]'
-rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[1.0, -1.0, 0.0]' '[0.0, 0.0, 0.0]'
-
-rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.5, 1.0, 0.0]' '[0.0, 0.0, 0.0]'
-rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.35, -1.2, 0.0]' '[0.0, 0.0, 0.0]'
+#movement for 1st A
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[-0.32, -0.5, 0.0]' '[0.0, 0.0, 0.0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.96, 1.5, 0.0]' '[0.0, 0.0, 0.0]'
+#movement for M
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[1.0, -1.45, 0.0]' '[0.0, 0.0, 0.0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.5, 1.4, 0.0]' '[0.0, 0.0, 0.0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.35, -1.42, 0.0]' '[0.0, 0.0, 0.0]'
+#movement for 2nd A
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.5, 1.5, 0.0]' '[0.0, 0.0, 0.0]'
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.5, -1.5, 0.0]' '[0.0, 0.0, 0.0]'
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[-0.5, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
