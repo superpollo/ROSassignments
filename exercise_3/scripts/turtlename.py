8cf71db4137d4f79703c664b46b46ae155f17d7c
Alexander J. Magnusson Amoros <aj.magnussson@gmail.com>
2021-12-15 18:40:17 +0100

    Indentation fixed

--- exercise_3/scripts/turtlename.py
+++ exercise_3/scripts/turtlename.py
@@ -25,21 +25,21 @@ from geometry_msgs.msg import Twist
 
 
 linear = {
-	'x' : [0.0, 2.0, 3.0, 0.0, 8.0, 8.0, 0.0, -0.32, 
-		   0.96, 1.0, 0.5, 0.35, 0.5, 0.5, -0.5, 1.5],
-	'y' : [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5, 
-		   1.5, -1.45, 1.4, -1.42, 1.5, -1.5, 0.0, 0.0],
-	'z' : [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
-		   0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    'x' : [0.0, 2.0, 3.0, 0.0, 8.0, 8.0, 0.0, -0.32, 
+	   0.96, 1.0, 0.5, 0.35, 0.5, 0.5, -0.5, 1.5],
+    'y' : [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5, 
+	   1.5, -1.45, 1.4, -1.42, 1.5, -1.5, 0.0, 0.0],
+    'z' : [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+	   0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 }
 
 angular = {
-	'x' : [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
-		   0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-	'y' : [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
-		   0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-	'z' : [-1.5, 3.0, 0.0, 1.5, 7.5, 7.5, 0.7, 0.0, 
-		    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    'x' : [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+	   0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    'y' : [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+	   0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    'z' : [-1.5, 3.0, 0.0, 1.5, 7.5, 7.5, 0.7, 0.0, 
+	    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 }	
 
 arr_length = len(linear['x']) - 1
@@ -47,12 +47,12 @@ arr_length = len(linear['x']) - 1
 
 def turtlename():
 	
-	"""
+    """
     Description
     -----------
     Creates a publisher to "turtle1/cmd_vel" topic,
-	sending values for "Twist" linear and angular
-	parameters of turtle velocity.
+    sending values for "Twist" linear and angular
+    parameters of turtle velocity.
 
     Parameters
     ----------
@@ -60,54 +60,54 @@ def turtlename():
 
     Returns
     -------
-	None
+    None
 
-	"""
+    """
 
-	j = 0
+    j = 0
 
-	pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-	rospy.init_node('turtlename', anonymous=True)
-	rate = rospy.Rate(1)
-	pos_msg = Twist()
-	rate.sleep()
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('turtlename', anonymous=True)
+    rate = rospy.Rate(1)
+    pos_msg = Twist()
+    rate.sleep()
 
-	"""
-	Code clarification:
-	This first rate.sleep is put to give some time to the subscriber
-	to realize there is a new publisher publishing messages, making
-	it possible to receive all messages correctly. Before adding it,
-	the first message containing the first position coordinates was
-	not received.
-	"""
+    """
+    Code clarification:
+    This first rate.sleep is put to give some time to the subscriber
+    to realize there is a new publisher publishing messages, making
+    it possible to receive all messages correctly. Before adding it,
+    the first message containing the first position coordinates was
+    not received.
+    """
 
-	while not rospy.is_shutdown():
+    while not rospy.is_shutdown():
 
-		while j<=arr_length:
+	while j<=arr_length:
 
-			pos_msg.linear.x = linear['x'][j]
-			pos_msg.linear.y = linear['y'][j]
-			pos_msg.linear.z = linear['z'][j]
+		pos_msg.linear.x = linear['x'][j]
+		pos_msg.linear.y = linear['y'][j]
+		pos_msg.linear.z = linear['z'][j]
 
-			pos_msg.angular.x = angular['x'][j]
-			pos_msg.angular.y = angular['y'][j]
-			pos_msg.angular.z = angular['z'][j]
+		pos_msg.angular.x = angular['x'][j]
+		pos_msg.angular.y = angular['y'][j]
+		pos_msg.angular.z = angular['z'][j]
 
-			pub.publish(pos_msg)
+		pub.publish(pos_msg)
 
-			rate.sleep()
+		rate.sleep()
 
-			j = j+1
+		j = j+1
 		
 	
-		return
+	return
 
 
 if __name__ == '__main__':
 
-	try:
+    try:
 
-		turtlename()
-		rospy.signal_shutdown("Finished")
+	turtlename()
+	rospy.signal_shutdown("Finished")
 
-	except rospy.ROSInterruptException: pass
+    except rospy.ROSInterruptException: pass
